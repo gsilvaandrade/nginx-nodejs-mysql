a8162984cf039dc43c4994cf8e1f71b0bbadcd10
Gustavo Andrade <gustavos_andrade@hotmail.com>
2021-08-30 08:08:38 -0400

    updates

--- node/index.js
+++ node/index.js
@@ -5,22 +5,28 @@ const config = {
     host: 'db',
     user: 'root',
     password: 'root',
-    database:'nodedb'
+    database: 'nodedb'
 };
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
 console.log('conexão criada')
-
+const sqlDrop = `DROP TABLE IF EXISTS mensagem`
+connection.query(sqlDrop)
 const sqlCreate = `CREATE TABLE mensagem(id int not null auto_increment, texto varchar(50), primary key(id))`
 connection.query(sqlCreate)
-
 console.log('tabela criada')
-
 const sqlInsert = `INSERT INTO mensagem(texto) values('Code.education Rocks!')`
-connection.query(sqlInsert)
 
+connection.query(sqlInsert)
+const sqlInsert2 = `INSERT INTO mensagem(texto) values('Gustavo da Silva Andrade!')`
+connection.query(sqlInsert2)
+const sqlInsert3 = `INSERT INTO mensagem(texto) values('Wesley!')`
+connection.query(sqlInsert3)
 console.log('insert realizado')
+
+let html = ''
+
 
 const retorno = null;
 let resultado;
@@ -28,16 +34,19 @@ connection.query("SELECT * FROM mensagem", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
     resultado = JSON.parse(JSON.stringify(result))
-  });
-console.log('select realizado ')
+    for(let list in resultado){
+        console.log('list ', list)
+        html += '<h1>'+resultado[list].texto+'</h1>'
+    }
+});
 
 connection.end()
 
 
-app.get('/', (req,res) => {
-    res.send('<h1>'+(resultado[0].texto)+'</h1>')
+app.get('/', (req, res) => {
+    res.send(html)
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('Rodando na porta ' + port)
 })
